6f4b669e1b2353816e12d33c8574433c211ff841
NellaLB <Ornella.LE-BRAS@eleve.egd.mg>
2024-06-17 13:59:35 +0200

    Panda Practice

--- test.py
+++ test.py
@@ -6,11 +6,10 @@ pd.DataFrame(dataframe)
 
 #Cleaning data
 for row in dataframe.index:
-    serie = list(dataframe.iloc[row])
+    serie = list(dataframe.loc[row])
     for nbCol in range(len(serie)):
-        if serie[nbCol].isnumeric():
-            serie[nbCol]=None
-            dataframe.loc[row] = pd.Series(serie)
+        if serie[nbCol].strip().isnumeric() or serie[nbCol] == " ":
+            dataframe.drop(labels=row, inplace = True)
 dataframe.drop_duplicates(inplace=True)
 dataframe.dropna(inplace = True)
 dataframe.reset_index(inplace = True, drop = True)
