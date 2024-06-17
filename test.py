651fbd9a12987bab964b2033c8bcbcd2821de4d4
NellaLB <Ornella.LE-BRAS@eleve.egd.mg>
2024-06-17 12:28:26 +0200

    Panda Pratique

--- test.py
+++ test.py
@@ -5,15 +5,12 @@ dataframe = pd.read_csv('listeEnfants.csv')
 pd.DataFrame(dataframe)
 
 #Cleaning data
-for column in dataframe.columns:
-    serie = dataframe[column]
-    for nbRow in range(serie.size):
-        try:
-            serie[nbRow].astype(int)
-            serie[nbRow]=None
-            dataframe[column] = serie
-        except:
-            pass
+for row in dataframe.index:
+    serie = list(dataframe.iloc[row])
+    for nbCol in range(len(serie)):
+        if serie[nbCol].isnumeric():
+            serie[nbCol]=None
+            dataframe.loc[row] = pd.Series(serie)
 dataframe.drop_duplicates(inplace=True)
 dataframe.dropna(inplace = True)
 dataframe.reset_index(inplace = True, drop = True)
